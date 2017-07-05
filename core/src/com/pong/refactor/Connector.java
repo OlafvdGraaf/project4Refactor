bea82c83908637b1fcc2257624e746b3d784482b
Mischa van Dalen <omekeki@users.noreply.github.com>
2017-07-05 10:58:53 +0200

    connector test

--- core/src/com/pong/refactor/Connector.java
+++ core/src/com/pong/refactor/Connector.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
 import com.badlogic.gdx.maps.Map;
 import com.badlogic.gdx.maps.tiled.BaseTmxMapLoader;
+import com.badlogic.gdx.net.HttpParametersUtils;
 import com.badlogic.gdx.net.HttpRequestBuilder;
 
 import java.security.Policy;
+import java.util.HashMap;
 
 /**
  * Created by mischavandalen on 05/07/2017.
@@ -21,17 +23,20 @@ public class Connector {
 
     public Connector(){
         this.Requestbuilder = new HttpRequestBuilder();
-        this.Httprequest = this.Requestbuilder.newRequest().method(Net.HttpMethods.POST).url("http://nekonekochan.org/pong_request.php").build();
-        this.Httprequest.setHeader("Content-Type", "application/x-www-form-urlencoded");
-        this.Httprequest.setTimeOut(6000);
+        Items = new HashMap<String, String>();
     }
     public void Save_score(String Name, Integer Score){
         Items.put("name", Name);
         Items.put("score", String.format("%s", Score));
-        this.Requestbuilder.content("AAA");
+        this.Httprequest = this.Requestbuilder.newRequest().method(Net.HttpMethods.POST).url("http://nekonekochan.org/pong_request.php").content(HttpParametersUtils.convertHttpParameters(Items)).build();
+        this.Httprequest.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        this.Httprequest.setTimeOut(6000);
         this.Httprequest.reset();
     }
     public String Get_score() {
+        this.Httprequest = this.Requestbuilder.newRequest().method(Net.HttpMethods.POST).url("http://nekonekochan.org/pong_request.php").build();
+        this.Httprequest.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        this.Httprequest.setTimeOut(6000);
         Gdx.net.sendHttpRequest(this.Httprequest, new Net.HttpResponseListener() {
             @Override
             public void handleHttpResponse(Net.HttpResponse httpResponse) {
