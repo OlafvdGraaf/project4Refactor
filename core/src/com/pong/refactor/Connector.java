13ddeabd64a72d06a6b4af71c79091af36f40f06
Mischa van Dalen <omekeki@users.noreply.github.com>
2017-07-05 11:13:26 +0200

    fix this?

--- core/src/com/pong/refactor/Connector.java
+++ core/src/com/pong/refactor/Connector.java
@@ -25,13 +25,28 @@ public class Connector {
         this.Requestbuilder = new HttpRequestBuilder();
         Items = new HashMap<String, String>();
     }
-    public void Save_score(String Name, Integer Score){
+    public String Save_score(String Name, Integer Score){
         Items.put("name", Name);
         Items.put("score", String.format("%s", Score));
         this.Httprequest = this.Requestbuilder.newRequest().method(Net.HttpMethods.POST).url("http://nekonekochan.org/pong_save.php").content(HttpParametersUtils.convertHttpParameters(Items)).build();
         this.Httprequest.setHeader("Content-Type", "application/x-www-form-urlencoded");
         this.Httprequest.setTimeOut(6000);
+        Gdx.net.sendHttpRequest(this.Httprequest, new Net.HttpResponseListener() {
+            @Override
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                item = httpResponse.getResultAsString();
+            }
+            @Override
+            public void failed(Throwable t) {
+                item = "failed to retrieve data";
+            }
+            @Override
+            public void cancelled() {
+                item = "cancelled conntection";
+            }
+        });
         this.Httprequest.reset();
+        return item;
     }
     public String Get_score() {
         this.Httprequest = this.Requestbuilder.newRequest().method(Net.HttpMethods.POST).url("http://nekonekochan.org/pong_request.php").build();
